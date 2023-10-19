974a3eff45b032701f5e92d1e36ac68b4ecf8376
Bastian <bastian.guerrero.a@usach.cl>
2023-10-19 02:21:15 -0300

    Base 2.0

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUser)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 
 Vue.config.productionTip = false
 
@@ -8,3 +25,6 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+
+import 'bootstrap/dist/js/bootstrap.js'
