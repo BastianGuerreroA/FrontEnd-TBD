c8db64d2d9bc4e635aaf209092f5ef7a3258139c
RobertoGalleguillos <roberto.galleguillos@usach.cl>
2023-11-19 15:44:40 -0300

    regiones y tareas

--- frontend/src/components/Lmap.vue
+++ frontend/src/components/Lmap.vue
@@ -2,7 +2,7 @@
   <div>
     <select class="form-select form-select-lg mb-3" name="select-location" id="select-location" @change="updateMap">
       <option value="-1">Seleccione un lugar</option>
-      <option value="-33.45694,-70.64827">Santiago</option>
+      <option v-for="(region, index) in regions" :key="index" :value="`${region.center.lat},${region.center.lng}`">{{ region.name }}</option>
     </select>
     <div ref="map" style="height: 400px;"></div>
   </div>
@@ -14,23 +14,21 @@ import L from 'leaflet';
 import 'leaflet-minimap/dist/Control.MiniMap.min.css';
 import 'leaflet-minimap';
 import axios from "axios";
+
 export default {
   name: 'LmapComponent',
   data() {
     return {
       map: null,
       minimap: null,
-      regions: []
+      regions: [],
+      tareas: []
     };
   },
   mounted() {
     this.fetchRegionData();
     this.initializeMap();
-    
-    this.agregaMarcadores();
-    
-    
-
+    this.fetchTareaData();
   },
   methods: {
     initializeMap() {
@@ -42,37 +40,44 @@ export default {
         attribution: '© OpenStreetMap contributors',
       }).addTo(this.map);
 
-      // Crar minimap
+      // Crear minimap
       this.minimap = new L.Control.MiniMap(
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'),
-        {
-          toggleDisplay: true,
-          minimized: false,
-          position: 'bottomleft'
-        },
+          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'),
+          {
+            toggleDisplay: true,
+            minimized: false,
+            position: 'bottomleft'
+          },
       ).addTo(this.map);
-
-
     },
     fetchRegionData() {
       axios.get("http://localhost:8091/api/region")
           .then(response => {
-            this.regions = response.data;
+            this.regions = response.data.map(region => {
+              const invertedCoordinates = region.geom.coordinates[0].map(point => [point[1], point[0]]);
+              const center = this.calculatePolygonCenter(invertedCoordinates);
+              return {
+                ...region,
+                invertedCoordinates,
+                center
+              };
+            });
             console.log(response.data);
             console.log(this.regions);
             this.addChilePolygons();
-            
           })
           .catch(error => {
             console.error("Error fetching region data:", error);
           });
     },
+    calculatePolygonCenter(coordinates) {
+      const bounds = L.latLngBounds(coordinates);
+      return bounds.getCenter();
+    },
     addChilePolygons() {
       // Añadir polígonos al mapa utilizando los datos almacenados en la variable 'regions'
-      
       this.regions.forEach(region => {
-        const invertedCoordinates = region.geom.coordinates[0].map(point => [point[1], point[0]]);
-        const polygon = L.polygon(invertedCoordinates, { color: 'green' }).addTo(this.map);
+        const polygon = L.polygon(region.invertedCoordinates, { color: 'green' }).addTo(this.map);
         console.log("lo metimos");
         polygon.bindPopup(region.name);
       });
@@ -83,20 +88,35 @@ export default {
         // Si se selecciona la opción predeterminada, centrar en Chile
         this.map.flyTo([-35.6751, -71.543], 6, { duration: 1.5 });
       } else {
-        // Si se selecciona una ubicación específica, va centrar en esa ubicación con flyTo(Te dirije a la ubicacion seleccionada y de manera suave)
+        // Si se selecciona una ubicación específica, va centrar en esa ubicación con flyTo(Te dirige a la ubicacion seleccionada y de manera suave)
         const [lat, lng] = selectedLocation.split(',').map(Number);
-        this.map.flyTo([lat, lng], 13, { duration: 1.5 });
+        this.map.flyTo([lat, lng], 8, { duration: 1.5 });
       }
     },
-
+    fetchTareaData() {
+      axios.get("http://localhost:8091/api/tarea")
+          .then(response => {
+            this.tareas = response.data;
+            console.log(response.data);
+            console.log(this.tareas);
+            this.agregaMarcadores();
+          })
+          .catch(error => {
+            console.error("Error fetching tarea data:", error);
+          });
+    },
     agregaMarcadores() {
-      //Agrega marcadores al mapa
-      L.circleMarker(L.latLng(-33.45694, -70.64827), {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: 5,
-      }).addTo(this.map);
+      this.tareas.forEach(tarea => {
+        const estado = tarea.estado_tarea ? 'Activo' : 'Solucionado';
+        const marker = L.circleMarker(L.latLng(tarea.latitud, tarea.longitud), {
+          color: 'red',
+          fillColor: '#f03',
+          fillOpacity: 0.5,
+          radius: 5,
+        }).addTo(this.map);
+
+        marker.bindPopup(`<strong>Asunto: ${tarea.asunto_tarea}</strong><br>Estado: ${estado}`);
+      });
     },
   },
 };
@@ -104,4 +124,3 @@ export default {
 
 <style scoped>
 </style>
-
